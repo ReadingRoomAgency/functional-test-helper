ea0d3bbda42104447c2c9364495b9526f7895932
soundobj <soundobj@yahoo.com>
2017-08-24 10:41:14 +0100

    feature/addFeatureFiles  look into dependent project for configuration. dependent project requires batch file CMD for windows or shell script to execute index.js in the dependent project. TODO: check that npm is creating the cmd and bash commands automatically on install

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.error('@ INSIDE INDEX', null);
+// get dependent package.json 
+const packageJson = require(`${process.cwd()}\\package.json`) // eslint-disable-line
+
+console.error('@ INSIDE INDEX', process.cwd());
+console.error('@ DIRNAME', __dirname);
+console.error('@ JSON', packageJson.ftHelper);
+
+if (packageJson && packageJson.ftHelper) {
+  
+} else {
+  console.error(
+  `functional-test-helper 
+  needs a "ftHelper" configuration 
+  property in your package json`);
+  process.exit(1);
+}
